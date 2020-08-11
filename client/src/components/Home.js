7c40a714d76f9ba2106ade2df836f9695fc6db80
SeanPower <seanmp22@gmail.com>
2020-08-11 16:54:36 -0400

    Table on home page installed and displaying data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import ReactTable from "react-table-v6"
+import "react-table-v6/react-table.css"
+
 
 
 class Home extends React.Component {
@@ -26,6 +29,69 @@ class Home extends React.Component {
 
 
   render() {
+    const columns = [
+      {
+        Header: "Provider",
+        accessor: "providerName"
+      },
+      {
+        Header: "Program",
+        accessor: "program"
+      },
+      {
+        Header: "Certification/Credential",
+        accessor: "certification"
+      },
+      {
+        Header: "State",
+        accessor: "state"
+      },
+      {
+        Header: "County",
+        accessor: "region"
+      },
+      {
+        Header: "Modality",
+        accessor: "modality"
+      },
+      {
+        Header: "Cost",
+        accessor: "price"
+      },
+      {
+        Header: "Pell Grant Eligible?",
+        accessor: "pell"
+      },
+      {
+        Header: "VT Grant Eligibile?",
+        accessor: "VTGrant"
+      },
+      {
+        Header: "Start Date",
+        accessor: "startDate"
+      },
+      {
+        Header: "End Date",
+        accessor: "endDate"
+      },
+      {
+        Header: "Website",
+        accessor: "providerLink"
+      },
+      {
+        Header: "Contact Email",
+        accessor: "contactEmail"
+      },
+      {
+        Header: "Record Created By",
+        accessor: "recordCreatedBy"
+      },
+      {
+        Header: "Last Updated",
+        accessor: "lastUpdate"
+      }
+    ]
+
     return (
       <div className="main-container" >
         <h1>List of Non-Degree and Training Programs</h1>
@@ -33,11 +99,12 @@ class Home extends React.Component {
 
         </div>
         <div id='db-info-container'>
-          {/* <table id="programTable">
-            <tr class="header">
-              <th style={{width: 10%}}>Provider</th>
-            </tr>
-          </table> */}
+          <ReactTable
+            columns={columns}
+            data={this.state.programs}  
+          >
+
+          </ReactTable>
         </div>
       </div>
     );
